b7ecb174541b40c8fbbde6a611c68ceecf5f6be4
DB <david.bencek@monopolkommission.bund.de>
2020-12-10 17:37:01 +0100

    export main function

--- R/acf.R
+++ R/acf.R
@@ -1,3 +1,12 @@
+#' Title
+#'
+#' @param initial_values named numeric
+#' @param data data frame
+#' @param ... additional arguments to be passed to `optim()`
+#'
+#' @return list
+#' @export
+#'
 acf <- function(initial_values, data, ...) {
   result <- stats::optim(par = initial_values, fn = acf_gmm, data = data, ...)
   return(result$par)
