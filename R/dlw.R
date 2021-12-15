a92cb9ddfc9fb3d8bd3647eaef2251e88c3d985f
David Benček <david.bencek@monopolkommission.bund.de>
2021-12-15 15:43:53 +0100

    added function for DLW-type estimation

---
+++ R/dlw.R
@@ -0,0 +1,81 @@
+#' Estimate a structural value-added production function (as in DLW)
+#'
+#' @description
+#' Using the two-stage proxy function approach as described by De Loecker and
+#' Warzynski (2012), based on Ackerberg, Caves, and Frazer (2015),
+#' estimate a Cobb-Douglas or translog structural value-added production function.
+#' The sole difference to the `acf()`-function is the instrumentation in the second
+#' stage GMM-estimation which also omits the intercept term.
+#'
+#' @param initial_values A named numeric vector of initial values for the GMM-procedure.
+#' @param data A data frame of input factors and production output. Variable names need to follow a specific convention, see below.
+#' @param type A string specifying the type of production function to be estimated, either "cd" for Cobb-Douglas or "tl" for translog.
+#' @param ... additional arguments to be passed to `optim()` and the moment evaluator function. See Details for more information.
+#'
+#' @return A list with components (see [stats::optim()]):
+#'
+#' * `par` A vector of best parameter estimates.
+#' * `value` The minimized moment function value.
+#' * `counts` The number of calls to fn and gr.
+#' * `convergence` An integer code. 0 indicates sucessful completion.
+#' * `message` A character string potentially giving additional information.
+#'
+#' @importFrom stats optim
+#' @export
+#'
+#' @details
+#' ## Estimation
+#' The implemented function uses a third-order polynomial to approximate the Markov process
+#' of productivity growth.
+#'
+#' ## Initial values
+#' The choice of initial values can significantly influence estimation results.
+#' It is recommended to run estimations on a range of combinations of initial values.
+#' Initial values must be provided for each coefficient of the underlying production function.
+#' For a Cobb-Douglas type, this requires two initial values, `c(bl = 0.4, bk = 0.6)`.
+#' It is best practice to provide a named vector to avoid unnecessary, and potentially hard to debug mistakes.
+#' Without any names, the estimation routine assumes the order of initial values to be (1) labour,
+#' and (2) capital coefficient.
+#'
+#' ## Instruments
+#' The moment evaluator function by default uses lagged labour as an instrument for labour as well as a constant, capital, and lagged phi.
+#' In order to manually specify the instruments to be used in the GMM-stage, they can be added via the `instruments`-argument
+#' as a character vector of variable names which gets passed to the moment function.
+#' For example, adding same-period labour as an instrument is done using `instruments = c("l", "l_lag", "k")`
+#' in the `dlw()`-function.
+#'
+#' ## Data
+#' Variable names are hard-coded in the estimation procedures of this package and follow conventions in the literature.
+#' Depending on the functional type, the following variables as well as their lags are required (sufficient for Cobb-Douglas in bold):
+#' * **y** (log output)
+#' * **l** (log labour)
+#' * **k** (log capital)
+#' * **phi** (fitted value from a first-stage approximation)
+#' * ll (squared log labour)
+#' * kk (square log capital)
+#' * lk (interaction of log labour and log capital)
+#' * l_lag_k (interaction of lagged log labour and current log capital, no further lag required)
+#'
+#' @references
+#' Ackerberg, D. A., Caves, K., & Frazer, G. (2015). Identification Properties of Recent Production Function Estimators. Econometrica, 83(6), 2411–2451. https://doi.org/10.3982/ECTA13408
+#'
+#' De Loecker, J., & Warzynski, F. (2012). Markups and Firm-Level Export Status. American Economic Review 102(6), 2437-2471. https://doi.org/10.1257/aer.102.6.2437
+
+
+dlw <- function(initial_values, data, type = c("cd", "tl"), ...) {
+  type <- match.arg(type)
+
+  if (type == "cd") {
+    if (length(initial_values) != 2) stop("Estimating a Cobb-Douglas function requires initial values for bl, and bk.")
+    initial_values <- check_initial_values(x = initial_values, type = "cd", version = "dlw")
+    result <- stats::optim(par = initial_values, fn = dlw_gmm_cd, data = data, ...)
+  }
+
+  if (type == "tl") {
+    if (length(initial_values) != 5) stop("Estimating a Translog function requires initial values for bl, bk, bll, bkk, and blk.")
+    initial_values <- check_initial_values(x = initial_values, type = "tl", version = "dlw")
+    result <- stats::optim(par = initial_values, fn = dlw_gmm_tl, data = data, ...)
+  }
+
+  return(result)
+}
---
+++ R/dlw_gmm.R
@@ -0,0 +1,77 @@
+#' Second Stage GMM (as in DLW)
+#'
+#' Evaluate the moment conditions of a Cobb-Douglas production function.
+#'
+#' @param param numeric
+#' @param data data frame
+#' @param instruments character
+#'
+#' @return numeric
+#' @importFrom stats complete.cases
+acf_gmm_cd <- function(param, data, instruments = c("l_lag", "k")){
+  data <- data[stats::complete.cases(data[, instruments]), ]
+
+  if (!exists("const", data)) data$const <- 1
+
+  PHI <- as.matrix(data[, "phi"])
+  PHI_lag <- as.matrix(data[, "phi_lag"])
+  Z <- as.matrix(data[, instruments])
+
+  X <- as.matrix(data[, c("l", "k")])
+  X_lag <- as.matrix(data[, c("l_lag", "k_lag")])
+
+  C <- as.matrix(data[, "const"])
+  betas <- as.matrix(c(param["bl"], param["bk"]), ncol = 1)
+
+  OMEGA <- PHI - X %*% betas
+  OMEGA_lag <- PHI_lag - X_lag %*% betas
+
+  OMEGA_lag_pol <- cbind(OMEGA_lag, OMEGA_lag^2, OMEGA_lag^3, C)
+
+  g_b <- solve(t(OMEGA_lag_pol) %*% OMEGA_lag_pol) %*% t(OMEGA_lag_pol) %*% OMEGA
+  XI <- OMEGA - OMEGA_lag_pol %*% g_b
+
+  crit <- t(t(Z) %*% XI)  %*%  (t(Z) %*% XI)
+
+  return(crit)
+}
+
+
+
+#' Second Stage GMM (as in DLW)
+#'
+#' Evaluate the moment conditions of a Translog production function.
+#'
+#' @param param numeric
+#' @param data data frame
+#' @param instruments character
+#'
+#' @return numeric
+#' @importFrom stats complete.cases
+acf_gmm_tl <- function(param, data, instruments = c("l_lag", "k", "ll_lag", "kk", "l_lag_k")){
+  data <- data[stats::complete.cases(data[, instruments]), ]
+
+  if (!exists("const", data)) data$const <- 1
+
+  PHI <- as.matrix(data[, "phi"])
+  PHI_lag <- as.matrix(data[, "phi_lag"])
+  Z <- as.matrix(data[, instruments])
+
+  X <- as.matrix(data[, c("l", "k", "ll", "kk", "lk")])
+  X_lag <- as.matrix(data[, c("l_lag", "k_lag",  "ll_lag", "kk_lag", "lk_lag")])
+
+  C <- as.matrix(data[, "const"])
+  betas <- as.matrix(c(param["bl"], param["bk"], param["bll"], param["bkk"], param["blk"]),  ncol = 1)
+
+  OMEGA <- PHI - X %*% betas
+  OMEGA_lag <- PHI_lag - X_lag %*% betas
+
+  OMEGA_lag_pol <- cbind(OMEGA_lag, OMEGA_lag^2, OMEGA_lag^3, C)
+
+  g_b <- solve(t(OMEGA_lag_pol) %*% OMEGA_lag_pol) %*% t(OMEGA_lag_pol) %*% OMEGA
+  XI <- OMEGA - OMEGA_lag_pol %*% g_b
+
+  crit <- t(t(Z) %*% XI)  %*%  (t(Z) %*% XI)
+
+  return(crit)
+}
